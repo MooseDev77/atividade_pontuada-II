3fd65fa0fc47c492a9a09ef628174a89cb20cdf4
MooseDev77 <luigisantos.oficial@gmail.com>
2025-04-02 19:53:52 +0000

    incompleto

--- Cardapio_Gourmet Tech.py
+++ Cardapio_Gourmet Tech.py
@@ -1,62 +1,64 @@
 import os 
 os.system("clear")
-
+subtotal= 0
+pedido = 0
+forma_pagamento = 0
+nome = str ()
+preco = ()
+soma = 0
 #O cardapio das comidas da casa
-menu = ("""          
-   1:  ("Acarajé", 8.00)
-   2:  ("Moqueca Bahiana", 104.90 ("seve duas pessoas"))
-   3:  ("Vatapá", 10.00" ("Pequena porção"))
-   4:  ("Abará", 8.00)
-   5:  ("Caruru", 25.00)
-   6:  ("Bobó de Camarão", 89.90 ("Serve duas pessoas"))
-   7:  ("Xinxim de Galinha", 30.00)
-   """)
+
+menu = {     
+    1:  ("Acarajé", 8.00),
+    2:  ("Moqueca Bahiana", 104.90),
+    3:  ("Vatapá", 10.00),
+    4:  ("Abará", 8.00),
+    5:  ("Caruru", 25.00),
+    6:  ("Bobó de Camarão", 89.90),
+    7:  ("Xinxim de Galinha", 30.00)
+}
+    
+  
 # Processo de escolha dos pratos
-pedido = ()
 while True:
-      print ("menu:")
-      for codigo, (nome, preco) in menu.items():
+        menu = codigo and nome and preco
         print(f"{codigo} - {nome}: R${preco:.2f}")
-   
-      codigo = int(input("Digite o código do prato escolhido (ou 0 para finalizar): "))
-      if codigo == 0:
-       break
-      elif codigo in menu:
-       pedido.append(menu[codigo])
-      else:
-         print("Código inválido. Tente novamente.")
-      continue
-#Cáculo do valor total
-subtotal = sum(preco for _, preco in pedido)
+        contador = +1
+        codigo += preco 
+        print("Menu:")
+        codigo = int(input("Digite o código do prato desejado (ou 0 para finalizar): "))
 
-# Escolha da forma de pagamento
-while True:
-   print("\nForma de pagamente:")
-   print("1 - À vista (10% de desconto)")
-   print("2 - Cartão de crédito (10% de acréscimo)")
-   opcao_pagamneto = int(input("Qual a forma de pagamento "))
-
-   match opcao_pagamneto:
-      case 1:
-         desconto = subtotal * 0.10
-         total = subtotal - desconto
-         forma_pagamento = "À vista"
-         break
-      case 2:
-         desconto = -subtotal * 0.10 #acréscimo
-         total = subtotal - desconto
-         forma_pagamento = "Cartão de Crédito"
-         break
-      case _:
-         print("Opção inválida. Escolha novamente.")
-
-# Mostrando Resultados Finais
-print("\nResumo do pedido: ")
+        if codigo == 0:
+            break
+        
+        else:
+            print("Código invalido. Tente novamente.")
+            continue
+        # Cáculo do valor total
+        subtotal = (preco, preco, pedido)
+        opcao_pagamento = int(input("Escolha a forma de pagamento: "))
+        print("\nForma de pagamento:")
+        print("1 - À vista (10% de desconto)")
+        print("2 - Cartão de crédito (10% de acréscimo)")
+#Escolha da forma de pagamento
+        match opcao_pagamento:
+            case 1:
+                desconto = subtotal * 0.10
+                total = subtotal - desconto
+                forma_pagamento = "À vista"
+                
+            case 2:
+                desconto = -subtotal * 0.10 # Acréscimo
+                total = subtotal - desconto
+                forma_pagamento = "Cartão de Crédito"
+                
+            case _:
+                print("Opção invalida. Escolha novamente.")
+# Exixbindo resultados finais
+print("\nResumo do pedido:")
 for nome, preco in pedido:
-   print(f" - {nome}: R${preco:.2f}")
+    print(f" - {pedido}: R${preco:.2f}")
 print(f"Subtotal: R${subtotal:.2f}")
 print(f"Forma de pagamento: {forma_pagamento}")
 print(f"Valor do desconto ou acréscimo: R${desconto:.2f}")
 print(f"valor final a ser pago: R${total:.2f}")
-
-      
